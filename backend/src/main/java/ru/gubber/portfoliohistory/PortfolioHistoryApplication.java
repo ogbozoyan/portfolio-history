9a6eaa90996336e2dc93838ea1224ee418d112f2
gubber <admin@gubber.ru>
2024-05-06 23:27:57 +0300

    initial commit

---
+++ backend/src/main/java/ru/gubber/portfoliohistory/PortfolioHistoryApplication.java
@@ -0,0 +1,13 @@
+package ru.gubber.portfoliohistory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioHistoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortfolioHistoryApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/ru/gubber/portfoliohistory/PortfolioHistoryApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.gubber.portfoliohistory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioHistoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
