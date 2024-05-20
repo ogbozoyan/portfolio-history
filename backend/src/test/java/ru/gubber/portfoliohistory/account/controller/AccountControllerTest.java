ec2ec808aee6980cd92b1d95ded2cc435275a6d6
Anna-Vashchenkova <anna.inkognito7@yandex.ru>
2024-05-20 13:55:05 +0300

    [PH-10] Tests AccountController.updateAccount();

--- backend/src/test/java/ru/gubber/portfoliohistory/account/controller/AccountControllerTest.java
+++ backend/src/test/java/ru/gubber/portfoliohistory/account/controller/AccountControllerTest.java
@@ -97,10 +97,50 @@ class AccountControllerTest {
     }
 
     @Test
-    @DisplayName("ри обновлении ")
-    void updateAccount() {
+    @DisplayName("Вызывается сервис")
+    void updateAccount_thenUseService() {
+        UUID uuid3 = UUID.fromString("f21c831f-9807-4de5-88c7-61cfe33e1c47");
+        Account account3 = new Account(uuid3, "БКС3", "БКС", "03");
+
+        Mockito.when(mockAccountService.updateAccount(uuid3.toString(),"БКС3", "БКС", "03")).thenReturn(account3.getId());
+
+        accountController.updateAccount(new IdIncomeAccountDto("f21c831f-9807-4de5-88c7-61cfe33e1c47"), new IncomeAccountDto("БКС3", "БКС", "03"));
+        verify(mockAccountService).updateAccount(uuid3.toString(),"БКС3", "БКС", "03");
     }
 
+    @Test
+    @DisplayName("При обновлении некорректных данных счета - idDto.isEmpty- возвращается ошибка валидации")
+    void updateAccount_whenIdDtoIsEmpty_thenReturnValidationError() {
+        List<FieldValidationError> responce = new ArrayList<>();
+        responce.add(new FieldValidationError("id","Поле не может быть пустым"));
+        ValidationError responceError = new ValidationError(ResponceStatus.ERROR, "Не правильный запрос", responce);
+        ValidationError result = (ValidationError) accountController.updateAccount(new IdIncomeAccountDto(""), new IncomeAccountDto("БКС3", "БКС", "03"));
+        Assertions.assertEquals(responceError.getErrorMessage(), result.getErrorMessage());
+    }
+
+    @Test
+    @DisplayName("При обновлении данных счета возвращается корректный UUID")
+    void updateAccount_whenDtoIsValid_thenReturnUUID() {
+        UUID uuid3 = UUID.fromString("f21c831f-9807-4de5-88c7-61cfe33e1c47");
+
+        Mockito.when(mockAccountService.updateAccount(anyString(), anyString(), anyString(), anyString())).thenReturn(uuid3);
+
+        IdOutcomeAccountDto responceResult = (IdOutcomeAccountDto) accountController.updateAccount(new IdIncomeAccountDto(uuid3.toString()), new IncomeAccountDto("БКС3", "БКС", "03"));
+        ResponceId responce = (ResponceId) responceResult.getResponce();
+        UUID resultUUID = responce.id();
+
+        Assertions.assertEquals(uuid3, resultUUID);
+    }
+
+    @Test
+    @DisplayName("При обновлении данных счета при получении из сервиса значения null возвращается Error")
+    void updateAccount_whenUUIDisNull_thenReturnError() {
+        Mockito.when(mockAccountService.updateAccount(anyString(), anyString(), anyString(), anyString())).thenReturn(null);
+        ValidationError validationError = new ValidationError(ResponceStatus.WARN,
+                String.format("Нет счёта с идентификатором %s", "f21c831f-9807-4de5-88c7-61cfe33e1c50"), null);
+        ValidationError result = (ValidationError) accountController.updateAccount(new IdIncomeAccountDto("f21c831f-9807-4de5-88c7-61cfe33e1c50"), new IncomeAccountDto("БКС3", "БКС", "БКС3"));
+        Assertions.assertEquals(validationError.getErrorMessage(), result.getErrorMessage());
+    }
     /*@Test
     void deleteAccount() {
     }*/
