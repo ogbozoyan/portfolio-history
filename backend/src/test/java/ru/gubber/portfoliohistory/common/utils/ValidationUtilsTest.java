91aebd4af54f9ae6f64b8221e8a480b7ed613346
Anna-Vashchenkova <anna.inkognito7@yandex.ru>
2024-05-25 11:20:10 +0300

    [PH-10] ValidationUtilsTest;

--- backend/src/test/java/ru/gubber/portfoliohistory/common/utils/ValidationUtilsTest.java
+++ backend/src/test/java/ru/gubber/portfoliohistory/common/utils/ValidationUtilsTest.java
@@ -1,5 +1,7 @@
 package ru.gubber.portfoliohistory.common.utils;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullSource;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -7,11 +9,19 @@ import org.junit.jupiter.params.provider.ValueSource;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ValidationUtilsTest {
+
     @ParameterizedTest(name = "Для строки {0} необходимо возвращать ошибку.")
     @NullSource
     @ValueSource(strings = { "", "                ", "\t" })
-    public void validateStringField_shouldReturnCorrectError(String str) {
+    void validateStringField_shouldReturnCorrectError(String str) {
         FieldValidationError fieldValidationError = ValidationUtils.validateStringField(str);
         assertNotNull(fieldValidationError);
+    }
+
+    @Test
+    @DisplayName("При непустой строке возвращается null")
+    void validateStringField_whenStringNotEmpty_thenReturnNull() {
+        FieldValidationError fieldValidationError = ValidationUtils.validateStringField("проверка");
+        assertNull(fieldValidationError);
     }
 }
