a93e908f7b0134a4489e433b477634e4b4993302
Anna-Vashchenkova <anna.inkognito7@yandex.ru>
2024-05-31 21:56:32 +0300

    [PH-10] OperationServiceImplTest;

---
+++ backend/src/test/java/ru/gubber/portfoliohistory/operation/controller/OperationControllerTest.java
@@ -0,0 +1,13 @@
+package ru.gubber.portfoliohistory.operation.controller;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OperationControllerTest {
+    UUID operationUUID = UUID.fromString("f99b9e41-4753-43ad-89cd-1874c3a35c90");
+    LocalDateTime creatTime = LocalDateTime.of(2024, 5, 31, 20, 30 );
+    UUID accoundUUID = UUID.fromString("f99b9e41-4753-43ad-89cd-1874c3a35c40");
+
+}
---
+++ backend/src/test/java/ru/gubber/portfoliohistory/operation/service/OperationServiceImplTest.java
@@ -0,0 +1,56 @@
+package ru.gubber.portfoliohistory.operation.service;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.*;
+import org.mockito.junit.jupiter.MockitoExtension;
+import ru.gubber.portfoliohistory.account.service.AccountService;
+import ru.gubber.portfoliohistory.asset.service.PurchasedAssetService;
+import ru.gubber.portfoliohistory.operation.model.Operation;
+import ru.gubber.portfoliohistory.operation.repository.OperationRepository;
+
+import java.util.UUID;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+class OperationServiceImplTest {
+    @InjectMocks
+    OperationServiceImpl operationService;
+    @Mock
+    OperationRepository mockRepository;
+    @Mock
+    AccountService mockAccountService;
+    @Mock
+    PurchasedAssetService mockPurchasedAssetService;
+    UUID accoundUUID = UUID.fromString("f99b9e41-4753-43ad-89cd-1874c3a35c40");
+    @Captor
+    ArgumentCaptor<Operation> operationArgumentCaptor;
+
+    @Test
+    @DisplayName("При существующем id счета возвращается uuid операции")
+    public void replenishAccount_whenAccountExists_thenReturnOperationUUID() {
+        double amount = 105.3;
+        Mockito.when(mockAccountService.accountExists(any())).thenReturn(true);
+        Mockito.when(mockRepository.save(any())).thenAnswer(invocationOnMock ->
+                invocationOnMock.getArgument(0));
+
+        UUID resultUuid = operationService.replenishAccount(accoundUUID.toString(), amount, 1.0);
+        verify(mockAccountService).setCurrentBalance(accoundUUID, amount);
+        verify(mockRepository).save(operationArgumentCaptor.capture());
+        Assertions.assertEquals(operationArgumentCaptor.getValue().getId(), resultUuid);
+    }
+
+    @Test
+    @DisplayName("При несуществующем id счета возвращается null")
+    public void replenishAccount_whenAccountNotExists_thenReturnNull() {
+        double amount = 105.3;
+        Mockito.when(mockAccountService.accountExists(any())).thenReturn(false);
+
+        UUID resultUuid = operationService.replenishAccount(accoundUUID.toString(), amount, 1.0);
+        Assertions.assertNull(resultUuid);
+    }
+}
